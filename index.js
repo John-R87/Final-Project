871a0cd50749e59b6ab73587bd64636845e85d1b
John-R87 <johnathan.rosales@outlook.com>
2026-07-24 15:38:25 -0700

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+async function onSearchChange(event) {
+    const title = event.target.value;
+getMovie(title)
+
 async function getMovie(title) {
-    
+    const response = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=89315988`)
 }
