9de065c0832793c9bc6dac7ddc1b02a53498efc8
John-R87 <johnathan.rosales@outlook.com>
2026-07-28 17:02:06 -0700

    api

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 async function onSearchChange(event) {
     const title = event.target.value;
-getMovie(title)
+getMovie(event)
 
-async function getMovie(title) {
-    const response = await fetch (`https://www.omdbapi.com/?i=tt3896198&apikey=89315988`)
+async function getMovie(event) {
+    const title = event.target.value;
+    const response = await fetch (
+        `https://www.omdbapi.com/?i=tt3896198&apikey=89315988?s=${title}` 
+    );
+    const data = await response.json();
+    console.log(data);
 }
